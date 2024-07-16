c74a9003cfc5ff15bbce8cdeef95f7a06792485a
Sergey <serg_zv@msn.com>
2024-07-16 10:33:33 +0300

    Загрузка

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,11 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCatHello() {
+        Cat helloWorld = new Cat();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
 }
